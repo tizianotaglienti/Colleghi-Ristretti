cd380fc81854542ade4ab640dc7b96dc4d4a4109
valeriocristofori <cristoforivalerio@gmail.com>
2020-05-18 16:59:28 +0200

    app conclusa

--- VectorAssetStudio/app/src/main/java/it/valeriocristofori/vectorassetstudio/activities/VectorActivity.java
+++ VectorAssetStudio/app/src/main/java/it/valeriocristofori/vectorassetstudio/activities/VectorActivity.java
@@ -1,8 +1,11 @@
 package it.valeriocristofori.vectorassetstudio.activities;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.chrisbanes.photoview.PhotoView;
 
 import it.valeriocristofori.vectorassetstudio.R;
 
@@ -12,6 +15,25 @@ public class VectorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_vector);
+        new Holder(this);
+    }
+
+    class Holder{
+
+        private Context context;
+        private PhotoView pvVector;
+
+        public Holder(Context context){
+
+            this.context = context;
+            this.pvVector = findViewById(R.id.pvVector);
+
+            this.pvVector.setImageResource(R.drawable.ic_android_logo_vector);
+
+
+
+        }
+
     }
 
 }
